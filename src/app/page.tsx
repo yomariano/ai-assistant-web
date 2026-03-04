e6b73461161568e9e5b749ca6189ea6edfbebf9d
alive4ever666 <yomariano05@gmail.com>
2026-03-04 20:39:38 -0300

    Revert "Add user event tracking, redesign hero with demo CTA, launch offer pricing"
    This reverts commit c253302a925b9b764cb58e2c4c45b03cd134f392.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,6 @@ import Header from "@/components/voicefleet/Header";
 import HeroSection from "@/components/voicefleet/HeroSection";
 import IntegrationBrandsCarousel from "@/components/voicefleet/IntegrationBrandsCarousel";
 import MetricsBar from "@/components/voicefleet/MetricsBar";
-import CountdownBanner from "@/components/voicefleet/CountdownBanner";
-import VideoShowcase from "@/components/voicefleet/VideoShowcase";
 
 // Below-fold components: lazy load for smaller initial bundle
 const ProblemSection = dynamic(() => import("@/components/voicefleet/ProblemSection"));
@@ -80,11 +78,9 @@ export default function Home() {
           },
         ]}
       />
-      <CountdownBanner />
       <Header />
       <main>
         <HeroSection />
-        <VideoShowcase />
         <IntegrationBrandsCarousel />
         <MetricsBar />
         <ProblemSection />
